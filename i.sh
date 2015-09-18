9d5c090e96b2112cf0ebd2e4e7ca7b8dd1c36e8e
Dmitrijs Milajevs <dimazest@gmail.com>
2015-09-18 14:39:11 +0000

    Update documentation

---
+++ i.sh
@@ -0,0 +1,4 @@
+curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > Miniconda3-latest-MacOSX-x86_64.sh
+sh Miniconda3-latest-MacOSX-x86_64.sh -b
+
+~/miniconda3/bin/conda install seaborn anaconda
