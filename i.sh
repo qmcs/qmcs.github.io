f9dd5fb3789095699beb4b237e756cddd4d2102e
Dmitrijs Milajevs <dimazest@gmail.com>
2015-09-20 16:03:39 +0000

    Update documentation

--- i.sh
+++
@@ -1,6 +0,0 @@
-curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > Miniconda3-latest-MacOSX-x86_64.sh
-sh Miniconda3-latest-MacOSX-x86_64.sh -b
-
-~/miniconda3/bin/conda install seaborn anaconda
-
-curl https://raw.githubusercontent.com/dimazest/ri-words/master/ri-words.ipynb > ri-words.ipynb
